119a1ae01c5478d5580be6ef6866a9053e6623b3
Berel Levy <berellevy@gmail.com>
2020-05-22 16:28:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,13 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning (file_path, emonnipon)
-  wonderful_array = load_library(file_path)
+  wonderful_hash = load_library(file_path)
+  wonderful_hash.each do |k_word_string, v_emos_hash|
+    if v_emos_hash[:japanese] = emonnipon
+      k_word_string
+    end
+    
+  end
   
   # code goes here
 end
