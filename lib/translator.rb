493756f3de44512d91620e5438f8844c01145ca4
Berel Levy <berellevy@gmail.com>
2020-05-22 16:19:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,14 @@ require "pry"
 def load_library (file_path)
   results_hash = {}
   hash_of_emos = YAML.load_file(file_path)
+  hash_of_emos.each do |k_word, v_array|
+    results_hash[k_word] = {
+      english: v_array[0]
+      japanese: v_array[1]
+    }
 
+
+  end
   binding.pry
   results_hash
 end
