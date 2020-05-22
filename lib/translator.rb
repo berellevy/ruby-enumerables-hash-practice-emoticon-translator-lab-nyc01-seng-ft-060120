09a1490225ed7ac9261e78f9c0fc56f9abb30365
Berel Levy <berellevy@gmail.com>
2020-05-22 16:29:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_english_meaning (file_path, emonnipon)
   wonderful_hash = load_library(file_path)
   wonderful_hash.each do |k_word_string, v_emos_hash|
     if v_emos_hash[:japanese] = emonnipon
-      k_word_string
+      return k_word_string
     end
 
   end
